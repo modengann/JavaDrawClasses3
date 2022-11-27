d3da0ae08b44acc32af34fd288d85270ae059c42
modengann <99417989+modengann@users.noreply.github.com>
2022-11-27 17:30:08 -0500

    init

---
+++ src/App.java
@@ -0,0 +1,26 @@
+/** Video 4: Creating and Dropping Bombs - 
+ * 
+ *  Task:
+ *  -Create Bomb class with following specifications
+ *      *has damage
+ *      *has speed
+ *      *can move
+ *  -write makeBomb method with following specifications:
+ *      *creates Bomb object at center of ship
+ *      *returns Bomb object
+ * 
+ * 
+ */
+
+import javadraw.*;
+import java.util.*;
+
+public class App extends Window{
+    public static void main(String[] args) throws Exception {
+        Window.open();
+    }
+
+    public void start(){
+
+    }
+}
