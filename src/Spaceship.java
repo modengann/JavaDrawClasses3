95d673a74947fe47a174eb4373dfed1978edad44
casey <cpmoden@gmail.com>
2022-11-27 17:39:40 -0500

    add tasks to spaceship

---
+++ src/Spaceship.java
@@ -0,0 +1,17 @@
+/** Video 4: Creating and Dropping Bombs - 
+ * 
+ *  Task:
+ *  -Create Bomb class with following specifications
+ *      *has damage
+ *      *has speed
+ *      *can move
+ *  -write makeBomb method with following specifications:
+ *      *creates Bomb object at center of ship
+ *      *returns Bomb object
+ * 
+ * 
+ */
+
+public class Spaceship {
+    
+}
